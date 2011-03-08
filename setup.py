5c13a002173666901cb995c4503b45eec093300d
shiva <me@shiv.me>
2011-03-08 14:04:04 +0800

    fixed setup.py error complaining about tagger.py not being found

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
 	description = "Python ID3 Tag Reader and Writer Module",
 	long_description = "An ID3v1 and ID3v2 tag reader and writer module in pure Python. Supports all standards including ID3v1, ID3v1.1, ID3v2.2, ID3v2.3 and ID3v2.4",
 	license = "BSD",
-	py_modules = ["tagger", "tagger.id3v1", "tagger.id3v2", "tagger.exceptions",
+	py_modules = ["tagger.id3v1", "tagger.id3v2", "tagger.exceptions",
 				  "tagger.constants", "tagger.utility", "tagger.id3v2frame",
 				  "tagger.encoding", "tagger.debug"],
     scripts = ["mp3check.py", "apic.py"]
